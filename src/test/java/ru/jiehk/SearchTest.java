87dfee9c7f2358a7d58811ecfac2153d0ec83de6
Tadree <32817585+Tadree@users.noreply.github.com>
2023-01-11 17:18:15 +0300

    Update SearchTest.java

--- src/test/java/ru/jiehk/SearchTest.java
+++ src/test/java/ru/jiehk/SearchTest.java
@@ -10,7 +10,7 @@ public class SearchTest {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[name=q]").setValue("vk").pressEnter();
+        $("[id=search]").shouldHave(text("https://vk.com"));
     }
 }

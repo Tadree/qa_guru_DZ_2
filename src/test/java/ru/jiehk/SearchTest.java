cbe66a5af5676f9694613626c2134fbd48b5b844
Elena Kosiakova <tadree@MacBook-Pro-Elena.local>
2023-01-11 17:07:29 +0300

    Commit from console

--- src/test/java/ru/jiehk/SearchTest.java
+++ src/test/java/ru/jiehk/SearchTest.java
@@ -1,2 +1,12 @@
-package ru.jiehk;public class SearchTest {
+package ru.jiehk;
+
+import org.junit.jupiter.api.Test;
+
+public class SearchTest {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
 }

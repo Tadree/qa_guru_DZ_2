a6d7153af68e62073db7293ad1108c9141c85cec
Elena Kosiakova <tadree@MacBook-Pro-Elena.local>
2023-01-11 17:09:03 +0300

    Commit from console

--- src/test/java/ru/jiehk/SearchTest.java
+++ src/test/java/ru/jiehk/SearchTest.java
@@ -2,6 +2,10 @@ package ru.jiehk;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class SearchTest {
     @Test
     void successfulSearchTest() {
